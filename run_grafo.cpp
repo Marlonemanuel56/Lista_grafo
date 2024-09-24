7367e0b4055f47b9a692eed66a6b0b2580bd8b49
Marlonemanuel56 <marlonemanuel56@gmail.com>
2024-09-24 17:33:37 -0300

    Create run_grafo.cpp

---
+++ run_grafo.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+class GrafoMatriz {
+public:
+    int numVertices;
+    vector<vector<int>> matrizAdj;
+
+    GrafoMatriz(int n) {
+        numVertices = n;
+        matrizAdj.resize(n, vector<int>(n, 0));
+    }
+
+    void adicionarAresta(int u, int v) {
+        matrizAdj[u][v] = 1;
+        matrizAdj[v][u] = 1;
+    }
+
+    
+    void exibirMatrizAdj() {
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                cout << matrizAdj[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+class GrafoLista {
+public:
+    int numVertices;
+    vector<vector<int>> listaAdj;
+
+    GrafoLista(int n) {
+        numVertices = n;
+        listaAdj.resize(n);
+    }
+
+    void adicionarAresta(int u, int v) {
+        listaAdj[u].push_back(v);
+    }
+    
+    void exibirListaAdj() {
+        for (int i = 0; i < numVertices; i++) {
+            cout << i << ": ";
+            for (int v : listaAdj[i]) {
+                cout << v << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+void carregarGrafo(string nomeArquivo, GrafoMatriz &grafoMatriz, GrafoLista &grafoLista) {
+    ifstream arquivo(nomeArquivo);
+    if (!arquivo.is_open()) {
+        cout << "Erro ao abrir o arquivo" << endl;
+        return;
+    }
+
+    int n; 
+    arquivo >> n;
+
+    grafoMatriz = GrafoMatriz(n);
+    grafoLista = GrafoLista(n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int peso;
+            arquivo >> peso;
+            if (peso != 0) {
+                grafoMatriz.adicionarAresta(i, j);
+                grafoLista.adicionarAresta(i, j);
+            }
+        }
+    }
+
+    arquivo.close();
+}
+
+
+bool bfs(const GrafoLista &grafo, int s, int t, vector<int> &caminho) {
+    vector<bool> visitado(grafo.numVertices, false);
+    vector<int> anterior(grafo.numVertices, -1);
+    queue<int> fila;
+
+    visitado[s] = true;
+    fila.push(s);
+
+    while (!fila.empty()) {
+        int v = fila.front();
+        fila.pop();
+
+        if (v == t) {
+            for (int at = t; at != -1; at = anterior[at]) {
+                caminho.push_back(at);
+            }
+            reverse(caminho.begin(), caminho.end());
+            return true;
+        }
+
+        for (int vizinho : grafo.listaAdj[v]) {
+            if (!visitado[vizinho]) {
+                visitado[vizinho] = true;
+                anterior[vizinho] = v;
+                fila.push(vizinho);
+            }
+        }
+    }
+    return false;
+}
+
+void imprimirCaminhoBFS(const GrafoLista &grafo, int s, int t) {
+    vector<int> caminho;
+    if (bfs(grafo, s, t, caminho)) {
+        cout << "Caminho do vertice " << s << " até o vertice " << t << ": ";
+        for (int v : caminho) {
+            cout << v << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "Não há caminho entre os vértices " << s << " e " << t << endl;
+    }
+}
+
+void dfs(const GrafoLista &grafo, int s) {
+    vector<bool> visitado(grafo.numVertices, false);
+    stack<int> pilha;
+
+    pilha.push(s);
+
+    while (!pilha.empty()) {
+        int v = pilha.top();
+        pilha.pop();
+
+        if (!visitado[v]) {
+            cout << v << " ";
+            visitado[v] = true;
+        }
+
+        for (int vizinho : grafo.listaAdj[v]) {
+            if (!visitado[vizinho]) {
+                pilha.push(vizinho);
+            }
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    GrafoMatriz grafoMatriz(0); 
+    GrafoLista grafoLista(0);
+    carregarGrafo("pcv4.txt", grafoMatriz, grafoLista);
+
+    cout << "Matriz de Adjacência:\n";
+    grafoMatriz.exibirMatrizAdj();
+
+    cout << "\nLista de Adjacência:\n";
+    grafoLista.exibirListaAdj();
+
+    int s = 0, t = 3;
+    cout << "\nBusca em Largura (BFS):\n";
+    imprimirCaminhoBFS(grafoLista, s, t);
+
+    cout << "\nBusca em Profundidade (DFS):\n";
+    dfs(grafoLista, s);
+
+    return 0;
+}
